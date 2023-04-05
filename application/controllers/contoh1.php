2cbc4269035d8fa500debea113591d13987d2a34
Ciptatel <ciptatelaumbanua@gmail.com>
2023-04-05 17:31:38 +0700

    menambahkan Latihan dan Modelnya

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class contoh1 extends CI_controller{
+
+    public function index(){
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Cipta saya tinggal di kemangisan olahraga yang saya sukai adalah Volly";
+    }
+}
